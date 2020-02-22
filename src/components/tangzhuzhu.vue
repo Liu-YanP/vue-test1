c15dae57dd7b478b0707ec53eb0fb46a6e53ac3e
Liu Yanping <liu199481@qq.com>
2020-02-22 12:32:41 +0800

    创建vue项目

---
+++ src/components/tangzhuzhu.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>    
+        <p>我是汤猪猪的</p><h1>{{msg}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    name:'zhuzhu',
+    data(){
+        return{
+            msg:'男朋友'
+        }
+    }
+}
+</script>
